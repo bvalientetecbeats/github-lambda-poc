d5e86c1a9bbf90edd031f12febe7997766a006c9
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-12 09:58:22 -0600

    Update initial code

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.DataFrame(data=d)
     r = requests.get('https://6a3fn4ovdc.execute-api.us-west-2.amazonaws.com/v2/platforms')
-    print(df)
     print('Hello POC!')
+    print(df)
     print('HTTP Request:')
     print(r.text)
