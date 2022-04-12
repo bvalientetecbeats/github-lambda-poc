d3432f32f8d5a336ca0adf93268a9622557c9767
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-12 16:48:51 -0600

    Updated Terraform state key and region

--- terraform-code/state.tf
+++ terraform-code/state.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "devops-terraform-state-poc-aw"
-    key            = "global/${var.service_type}/terraform.tfstate"
-    region         = var.aws_region
+    key            = "global/terraform.tfstate"
+    region         = "us-west-2"
     dynamodb_table = "devops-terraform-state"
     encrypt        = false
   }
