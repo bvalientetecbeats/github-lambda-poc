376630dc39ee487927f7a9a9a91207405cbaab8f
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-12 16:37:37 -0600

    Updated variable declaration and added terraform global lambda state

---
+++ terraform-code/state.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "state" {
+    bucket         = "devops-terraform-state-poc-aw"
+    key            = "global/${var.service_type}/terraform.tfstate"
+    region         = var.aws_region
+    dynamodb_table = "devops-terraform-state"
+    encrypt        = false
+  }
+}
--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -1,4 +1,14 @@
 variable "aws_region" {
   description = "The AWS region to deploy infrastructure"
-  default     = var.TF_REGION
+  default     = "${TF_REGION}"
+}
+
+variable "function_name" {
+  description = "The Lambda function name"
+  default     = "${TF_LAMBDA_FUNCTION_NAME}"
+}
+
+variable "service_type" {
+  description = "The service type"
+  default     = "${TF_SERVICE_TYPE}"  
 }
