c7eb0f989affe56ffe949e908eb8bf069af2cd5f
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-12 16:42:34 -0600

    Updated vars

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_region" {
   description = "The AWS region to deploy infrastructure"
-  default     = "${TF_REGION}"
+  default     = "#{codebuild.TF_REGION}"
 }
 
 variable "function_name" {
   description = "The Lambda function name"
-  default     = "${TF_LAMBDA_FUNCTION_NAME}"
+  default     = "#{codebuild.TF_LAMBDA_FUNCTION_NAME}"
 }
 
 variable "service_type" {
   description = "The service type"
-  default     = "${TF_SERVICE_TYPE}"  
+  default     = "#{codebuild.TF_SERVICE_TYPE}"
 }
