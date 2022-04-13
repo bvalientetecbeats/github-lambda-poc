b35c612921da574b366e67beedfdf6ea7d5e708d
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-13 14:28:28 -0600

    Updated backend bucket

--- terraform-code/state.tf
+++ terraform-code/state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-terraform-state-poc-aw"
+    bucket         = "devops-terraform-state-poc-levels"
     key            = "global/lambda/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "devops-terraform-state"
