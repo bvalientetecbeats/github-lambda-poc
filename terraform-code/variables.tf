c8e59147d613b4845238d2ed0c1f3dfdfa1907e1
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-12 19:06:17 -0600

    Updated vars syntax

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -1,29 +1,29 @@
 variable "aws_region" {
   description = "The AWS region to deploy infrastructure"
-  default     = "${codebuild.TF_VAR_REGION}"
+  default     = "#{TF_VAR_REGION}"
 }
 
 variable "function_name" {
   description = "The Lambda function name"
-  default     = "${codebuild.TF_VAR_LAMBDA_FUNCTION_NAME}"
+  default     = "#{TF_VAR_LAMBDA_FUNCTION_NAME}"
 }
 
 variable "service_type" {
   description = "The service type"
-  default     = "${codebuild.TF_VAR_SERVICE_TYPE}"
+  default     = "#{TF_VAR_SERVICE_TYPE}"
 }
 
 variable "runtime" {
   description = "Lambda runtime"
-  default     = "${codebuild.TF_VAR_LAMBDA_RUNTIME}"
+  default     = "#{TF_VAR_LAMBDA_RUNTIME}"
 }
 
 variable "handler" {
   description = "Lambda handler"
-  default     = "${codebuild.TF_VAR_LAMBDA_HANDLER}"
+  default     = "#{TF_VAR_LAMBDA_HANDLER}"
 }
 
 variable "build_number" {
   description = "The build number"
-  default     = "${CODEBUILD_BUILD_NUMBER}"
+  default     = "#{CODEBUILD_BUILD_NUMBER}"
 }
