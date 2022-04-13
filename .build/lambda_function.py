a8a34f9ca1bffb4b776d76b3c9c5051a247ca782
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-13 12:51:22 -0600

    Update lambda code

--- .build/lambda_function.py
+++ .build/lambda_function.py
@@ -5,4 +5,4 @@ def lambda_handler(event, context):
     r = http.request('GET','https://6a3fn4ovdc.execute-api.us-west-2.amazonaws.com/v2/platforms')
     print('Hello POC!')
     print('HTTP Request:')
-    print(r.text)
+    print(r.data)
