894414320d0473358c1e4e01df4dcd5975f2b28c
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-13 17:20:55 -0600

    Updated Lambda code

--- .build/lambda_function.py
+++ .build/lambda_function.py
@@ -1,7 +1,8 @@
 import urllib3
 
 def lambda_handler(event, context):
+    print('Hello POC!')
+    print()
     http = urllib3.PoolManager()
     r = http.request('GET','https://6a3fn4ovdc.execute-api.us-west-2.amazonaws.com/v2/platforms')
-    print('Hello POC!')
     print(r.data)
