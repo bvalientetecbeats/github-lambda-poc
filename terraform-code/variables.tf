196dc544119ba4af5d3e1cb7388003f73fd41312
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-12 21:06:03 -0600

    Updated variables, syntax and main

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -5,30 +5,24 @@ variable "aws_region" {
 
 variable "TF_VAR_REGION" {
   description = "The AWS region to use"
-  type = "string"  
 }
 
 variable "TF_VAR_LAMBDA_FUNCTION_NAME" {
   description = "The name of the Lambda function"
-  type = "string"  
 }
 
 variable "TF_VAR_SERVICE_TYPE" {
   description = "The type of service to create"
-  type = "string"
 }
 
 variable "TF_VAR_LAMBDA_RUNTIME" {
   description = "The runtime to use for the Lambda function"
-  type = "string"  
 }
 
 variable "TF_VAR_LAMBDA_HANDLER" {
-  description = "The handler to use for the Lambda function"
-  type = "string"  
+  description = "The handler to use for the Lambda function"  
 }
 
 variable "CODEBUILD_BUILD_NUMBER" {
   description = "The build number of the current build"
-  type = "string"  
 }
