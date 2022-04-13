bb648addaf42cb3912f87c3bde0ac88a6951458a
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-12 19:30:20 -0600

    Removed type from variables

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -1,35 +1,29 @@
 variable "aws_region" {
   description = "The AWS region to deploy infrastructure"
   default     = "#{TF_VAR_REGION}"
-  type        = "string"
 }
 
 variable "function_name" {
   description = "The Lambda function name"
   default     = "#{TF_VAR_LAMBDA_FUNCTION_NAME}"
-  type        = "string"
 }
 
 variable "service_type" {
   description = "The service type"
   default     = "#{TF_VAR_SERVICE_TYPE}"
-  type        = "string"
 }
 
 variable "runtime" {
   description = "Lambda runtime"
   default     = "#{TF_VAR_LAMBDA_RUNTIME}"
-  type        = "string"
 }
 
 variable "handler" {
   description = "Lambda handler"
   default     = "#{TF_VAR_LAMBDA_HANDLER}"
-  type        = "string"
 }
 
 variable "build_number" {
   description = "The build number"
   default     = "#{CODEBUILD_BUILD_NUMBER}"
-  type        = "number"
 }
