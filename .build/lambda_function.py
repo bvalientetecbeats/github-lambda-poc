4bb57373ea6a64644eeae4b269e53c0278d3898c
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-19 17:07:10 -0600

    Updated lambda code

--- .build/lambda_function.py
+++ .build/lambda_function.py
@@ -1,7 +1,7 @@
 import urllib3
 
 def lambda_handler(event, context):
-    print('Hello POC - From CircleCI')
+    print('Hello POC - CircleCI')
     print()
     http = urllib3.PoolManager()
     r = http.request('GET','https://6a3fn4ovdc.execute-api.us-west-2.amazonaws.com/v2/platforms')
