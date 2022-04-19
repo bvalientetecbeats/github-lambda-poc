8ff53c006fc44bbc84826d643f3db19835ad8e7d
Benjamin Valiente <benjaminvaliente@gmail.com>
2022-04-19 14:22:27 -0600

    Updated to use local variables

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -15,17 +15,17 @@ data "aws_iam_policy_document" "policy" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda_${var.lambda_function_name}"
+  name               = "iam_for_lambda_${local.lambda_function_name}"
   assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
 }
 
 resource "aws_lambda_function" "lambda" {
   filename         = "lambda_artifact.zip"
-  function_name    = var.lambda_function_name
+  function_name    = local.lambda_function_name
   role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = var.lambda_handler
+  handler          = local.lambda_handler
   source_code_hash = "${filebase64sha256("lambda_artifact.zip")}"
-  runtime          = var.lambda_runtime
+  runtime          = local.lambda_runtime
 
   environment {
     variables = {
