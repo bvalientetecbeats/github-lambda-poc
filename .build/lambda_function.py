a383e02350900f51118a9de9c0f582fada195568
bvalientetecbeats <bvaliente@tecbeats.com>
2022-04-26 04:59:10 -0600

    Updated lambda

--- .build/lambda_function.py
+++ .build/lambda_function.py
@@ -1,7 +1,7 @@
 import urllib3
 
 def lambda_handler(event, context):
-    print('Hello Oren - This is a change')
+    print('Hello POC!')
     print()
     http = urllib3.PoolManager()
     r = http.request('GET','https://6a3fn4ovdc.execute-api.us-west-2.amazonaws.com/v2/platforms')
