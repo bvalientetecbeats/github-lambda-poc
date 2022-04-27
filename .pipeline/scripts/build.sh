0e9d2bdfe379d6474e6013b05fb5fe46fc873c9b
bvalientetecbeats <bvaliente@tecbeats.com>
2022-04-27 05:58:12 -0600

    Updated build with s3

--- .pipeline/scripts/build.sh
+++ .pipeline/scripts/build.sh
@@ -5,5 +5,14 @@ apt -qq update -y && apt -qq install -y zip
 echo "Zipping deployment package..."
 cd ./git-repo/.build/
 zip lambda_artifact.zip lambda_function.py
-cp lambda_artifact.zip ../../workspace/
-ls -lah ../../workspace/
+echo "Setting up AWS environment..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -qq awscliv2.zip
+./aws/install
+aws --version
+echo "Setting up AWS credentials..."        
+aws configure set aws_access_key_id ((AWS_ACCESS_KEY_ID_PROD))
+aws configure set aws_secret_access_key ((AWS_SECRET_ACCESS_KEY_PROD))
+aws configure set default.region ((AWS_DEFAULT_REGION_PROD))
+echo "Uploading Lambda Artifact to S3..."
+cp lambda_artifact.zip s3://((s3_bucket_name))/lambda_artifact.zip
--- .pipeline/scripts/deploy_dev.sh
+++ .pipeline/scripts/deploy_dev.sh
@@ -10,13 +10,13 @@ unzip -qq awscliv2.zip
 ./aws/install
 aws --version
 echo "Setting up AWS credentials..."        
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID_DEV
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY_DEV
-aws configure set aws_session_token $AWS_SESSION_TOKEN_DEV
-aws configure set default.region $AWS_DEFAULT_REGION
+aws configure set aws_access_key_id ((AWS_ACCESS_KEY_ID_DEV))
+aws configure set aws_secret_access_key ((AWS_SECRET_ACCESS_KEY_DEV))
+aws configure set aws_session_token ((AWS_SESSION_TOKEN_DEV))
+aws configure set default.region ((AWS_DEFAULT_REGION))
 rm -rf awscliv2.zip terraform_1.1.8_linux_amd64.zip aws
 aws s3 ls
 echo "Deploying Lambda artifact..."
 cd ./.build/
-aws lambda update-function-code --function-name $VAR_LAMBDA_FUNCTION_NAME_DEV --zip-file fileb://lambda_artifact.zip --no-cli-pager
+aws lambda update-function-code --function-name ((VAR_LAMBDA_FUNCTION_NAME_DEV)) --zip-file fileb://lambda_artifact.zip --no-cli-pager
 echo "Lambda updated successfully to Development environment!"
