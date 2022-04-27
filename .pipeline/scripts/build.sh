1ba3326e8d0d793c85d7a767e008df78893f9787
bvalientetecbeats <bvaliente@tecbeats.com>
2022-04-27 05:40:04 -0600

    Updated build.sh

--- .pipeline/scripts/build.sh
+++ .pipeline/scripts/build.sh
@@ -5,3 +5,5 @@ apt -qq update -y && apt -qq install -y zip
 echo "Zipping deployment package..."
 cd ./git-repo/.build/
 zip lambda_artifact.zip lambda_function.py
+cp lambda_artifact.zip ../../workspace/
+ls -lah ../../workspace/
