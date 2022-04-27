d46874c9f29af27a8aaa461a2407ff2ccfa98256
bvalientetecbeats <bvaliente@tecbeats.com>
2022-04-27 06:09:54 -0600

    Updated script

--- .pipeline/scripts/build.sh
+++ .pipeline/scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Getting started with AWS Lambda deployment..."
 echo "Installing dependencies..."
-apt -qq update -y && apt -qq install -y zip
+apt -qq update -y && apt -qq install -y zip unzip curl
 echo "Zipping deployment package..."
 cd ./git-repo/.build/
 zip lambda_artifact.zip lambda_function.py
