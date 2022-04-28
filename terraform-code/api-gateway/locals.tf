884dec2a8505be958b1a4509fb61cb168a0de0a2
bvalientetecbeats <bvaliente@tecbeats.com>
2022-04-28 16:46:59 -0600

    Added API Gateway

--- terraform-code/api-gateway/locals.tf
+++ terraform-code/api-gateway/locals.tf
@@ -1,9 +1,6 @@
 locals {
   aws_region = "us-west-2"
-  service_type = "lambda"
-  lambda_runtime = "python3.8"
-  lambda_handler = "lambda_function.lambda_handler"
-  rest_api_domain_name = "example.com"
-  rest_api_name        = "api-gateway-rest-api-openapi-example"
+  rest_api_domain_name = "devops-api-gateway-poc"
+  rest_api_name        = "api-gateway-rest-api-openapi-poc"
   rest_api_path        = "/path1"
 }
