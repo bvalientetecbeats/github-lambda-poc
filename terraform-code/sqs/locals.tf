ab1bafe12c4263165611b06a140f6505a885c983
bvalientetecbeats <bvaliente@tecbeats.com>
2022-04-29 07:26:43 -0600

    SQS updated

--- terraform-code/sqs/locals.tf
+++ terraform-code/sqs/locals.tf
@@ -1,7 +1,8 @@
 locals {
-  aws_region      = "us-east-1"
-  sqs_name        = "devops-sqs-argyle-poc"
-  argyle_dlq_name = "devops-sqs-argyle-dlq-poc"
-  environment     = "test"
-  owner           = "devops-poc"
+  aws_region           = "us-east-1"
+  lambda_function_name = "lambdapoc"
+  sqs_name             = "devops-sqs-argyle-poc"
+  argyle_dlq_name      = "devops-sqs-argyle-dlq-poc"
+  environment          = "test"
+  owner                = "devops-poc"
 }
