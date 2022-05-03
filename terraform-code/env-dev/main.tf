f47ade6a6d143388d0033b4603bbeafd84b00638
bvalientetecbeats <bvaliente@tecbeats.com>
2022-05-03 07:12:11 -0600

    Initial changes

--- terraform-code/env-dev/main.tf
+++ terraform-code/env-dev/main.tf
@@ -1,8 +1,8 @@
 module "apigateway" {
   source               = "../modules/apigateway"
-  aws_region           = "us-west-2"
+  aws_region           = var.aws_region
   environment          = var.environment
-  owner                = "devops-tecbeats-poc"
+  owner                = var.tag_owner
   rest_api_domain_name = "devops-apigateway-dev"
   rest_api_name        = "api-gateway-restapi-dev"
   stage_name           = "stage_default"
@@ -11,21 +11,21 @@ module "apigateway" {
 
 module "lambda" {
   source               = "../modules/lambda"
-  aws_region           = "us-west-2"
+  aws_region           = var.aws_region
   environment          = var.environment
-  owner                = "devops-tecbeats-poc"
-  lambda_function_name = "Dev-ops-Lambda-1"
+  owner                = var.tag_owner
+  lambda_function_name = "lambdapoc"
   service_type         = "lambda"
-  lambda_runtime       = "python3.8"
+  lambda_runtime       = "nodejs14.x"
   lambda_handler       = "lambda_function.lambda_handler"
 }
 
 module "sqs" {
   source       = "../modules/sqs"
-  aws_region   = "us-west-2"
+  aws_region   = var.aws_region
   environment  = var.environment
-  owner        = "devops-poc"
-  sqs_name     = "devops-sqs-argyle-poc"
-  sqs_dlq_name = "devops-sqs-argyle-dlq-poc"
-  lambda_arn   = "arn:aws:lambda:us-west-2:416669478724:function:Dev-ops-Lambda-1"
+  owner        = var.tag_owner
+  sqs_name     = "devops-sqs-poc"
+  sqs_dlq_name = "devops-sqs-dlq-poc"
+  lambda_arn   = "arn:aws:lambda:us-east-1:451535584409:function:lambdapoc"
 }
--- terraform-code/env-dev/variables.tf
+++ terraform-code/env-dev/variables.tf
@@ -1,3 +1,12 @@
 variable "environment" {
     description = "The AWS environment"
 }
+
+variable "aws_region" {
+    description = "The AWS region"
+}
+
+variable "tag_owner" {
+    description = "Tag Owner value"
+    default = "devops-tecbeats-poc"  
+}
