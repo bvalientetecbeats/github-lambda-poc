7ec34c9fe52885a114c6c377929a51515d184b59
bvalientetecbeats <bvaliente@tecbeats.com>
2022-05-11 22:41:13 -0600

    Initial changes

--- terraform-code/dev/modules/apigateway/stages.tf
+++
@@ -1,5 +0,0 @@
-#resource "aws_api_gateway_stage" "stage_default" {
-#  deployment_id = aws_api_gateway_deployment.apigtw_deploy.id
-#  rest_api_id   = aws_api_gateway_rest_api.apigtw_restapi.id
-#  stage_name    = var.stage_name
-#}
--- terraform-code/dev/modules/sqs/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "sqs_url_data_source" {
-  value = aws_sqs_queue.sqs_dlq.url
-}
-
-output "sqs_url_resource_attribute" {
-  value = aws_sqs_queue.sqs_dlq.id
-}
