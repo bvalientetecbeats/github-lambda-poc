9123b6e9660ddb96d23f79381a02de691766dc2a
bvalientetecbeats <bvaliente@tecbeats.com>
2022-05-16 07:19:24 -0600

    Initial changes

---
+++ terraform-code/dev/ecs/modules/alb.tf
@@ -0,0 +1,41 @@
+# Application Load Balancer
+resource "aws_alb" "main" {
+  name            = "react-poc-alb"
+  #Public subnets level-public-a/b/c-subnet-dev
+  subnets         = ["subnet-0d59449d854dc3a0f","subnet-0855130bdc2795e94","subnet-068c79383d6173c35"]
+  security_groups = [aws_security_group.lb.id]
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_alb_target_group" "app" {
+  name        = "react-poc-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = var.health_check_path
+    unhealthy_threshold = "2"
+  }
+}
+
+# Redirect all traffic from the ALB to the target group
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.id
+  port              = var.app_port
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.app.id
+    type             = "forward"
+  }
+}
