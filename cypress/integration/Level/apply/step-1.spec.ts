e428b5708202f0de3aea9e19f8342657ac7b42af
bvalientetecbeats <bvaliente@tecbeats.com>
2022-05-25 23:22:47 -0600

    Initial code

--- cypress/integration/Level/apply/step-1.spec.ts
+++ cypress/integration/Level/apply/step-1.spec.ts
@@ -4,7 +4,7 @@ describe.only('Level Apply', () => {
   it('Layout 2000 x 760 ', () => {
     console.log('Hello World');
   });  
-  /*it('Layout 2000 x 769', () => {
+  it('Layout 2000 x 769', () => {
     cy.get('button[type=submit]').contains('Next Step')
-  });*/
+  });
 });
